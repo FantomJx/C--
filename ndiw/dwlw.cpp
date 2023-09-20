74a534e3306dc86bfa3fb85482976fd7fd17de35
FantomJx <markdanileychenko@gmail.com>
2023-09-20 23:25:06 +0300

    ауцтд

---
+++ ndiw/dwlw.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+    int a,b,c;
+    printf("WWrite numbers");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("%D, %D , %d", a,b,c);
+}
