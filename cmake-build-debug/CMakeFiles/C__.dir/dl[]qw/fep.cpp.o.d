0d34ebf9596ea11ca3279ef911144c7084ab8ce3
FantomJx <118744013+FantomJx@users.noreply.github.com>
2023-10-21 17:19:25 +0300

    cdecde

--- cmake-build-debug/CMakeFiles/C__.dir/dl[]qw/fep.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/C__.dir/dl[]qw/fep.cpp.o: \
-  /Users/mark/Documents/GitHub/C--/dl[]qw/fep.cpp
--- sort/sort.c
+++ sort/sort.c
@@ -9,7 +9,7 @@
 #define SORT(arr, n, type, comp) do { \
     for (int i = 0; i < n - 1; i++) { \
         for (int j = 0; j < n - i - 1; j++) { \
-            if (comp(arr[j], arr[j+1])) { \
+            if (arr[j] comp arr[j+1]) { \
                 SWAP(arr[j], arr[j+1], type); \
             } \
         } \
@@ -17,21 +17,25 @@
 } while (0)
 
 int main() {
-    int ascending_array[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-    int size = sizeof(ascending_array) / sizeof(ascending_array[0]);
-    SORT(ascending_array, size, int, >);
+    int asc_arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int size = sizeof(asc_arr) / sizeof(asc_arr[0]);
+
+    SORT(asc_arr, size, int, >);
+
     printf("After sorting in ascending order:\n");
     for (int i = 0; i < size; i++) {
-        printf("%d ", ascending_array[i]);
+        printf("%d ", asc_arr[i]);
     }
     printf("\n");
+    
     int des_arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
     size = sizeof(des_arr) / sizeof(des_arr[0]);
+
     SORT(des_arr, size, int, <);
+
     printf("After sorting in descending order:\n");
     for (int i = 0; i < size; i++) {
         printf("%d ", des_arr[i]);
     }
-    printf("\n");
     return 0;
 }
