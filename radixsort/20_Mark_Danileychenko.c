b0555ba92e92635e6b7ead15e9aab2241c9694f5
FantomJx <118744013+FantomJx@users.noreply.github.com>
2024-11-17 21:33:58 +0200

    hw 17.11

--- radixsort/20_Mark_Danileychenko.c
+++ radixsort/20_Mark_Danileychenko.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+// Аз го направих с последната, защото може да го делим само веднъж
 
-
-void printArray(int* arr, unsigned int count) {
-    for (unsigned int i = 0; i < count; i++) {
+void printArray(int* arr, int count) {
+    for (int i = 0; i < count; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-// Функция за намиране на максималния брой цифри в масива
-int getMaxDigits(int* arr, unsigned int count) {
+int mDigit(int* arr, int count) {
     int max = arr[0];
-    for (unsigned int i = 1; i < count; i++) {
+    for (int i = 1; i < count; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
@@ -25,60 +24,79 @@ int getMaxDigits(int* arr, unsigned int count) {
     return digits;
 }
 
-int getDigit(int number, int digitIndex) {
-    for (int i = 0; i < digitIndex; i++) {
+int get(int number, int digitInd) {
+    for (int i = 0; i < digitInd; i++) {
         number /= 10;
     }
     return number % 10;
 }
 
-void radix_sort(int* arr, unsigned int count, int digitIndex) {
-    if (count <= 1 || digitIndex < 0) {
+void radix_sort(int* arr, int count, int digitInd) {
+    if (count <= 1 || digitInd < 0) {
         return;
     }
 
-    printf("Сортиране по цифра на позиция %d:\n", digitIndex);
-    printArray(arr, count);
-
-    // Създаване на кофички за всяка цифра от 0 до 9
-    int* buckets[10];
-    int bucketCounts[10] = {0};
+    int* bucket[10];
+    int buckCount[10] = {0};
 
     for (int i = 0; i < 10; i++) {
-        buckets[i] = (int*)malloc(count * sizeof(int));
+        bucket[i] = (int*)malloc(count * sizeof(int));
     }
 
-    // Разпределяне на елементите в кофички според текущата цифра
-    for (unsigned int i = 0; i < count; i++) {
-        int digit = getDigit(arr[i], digitIndex);
-        buckets[digit][bucketCounts[digit]++] = arr[i];
+    for (int i = 0; i < count; i++) {
+        int digit = get(arr[i], digitInd);
+        bucket[digit][buckCount[digit]++] = arr[i];
     }
 
-    // Рекурсивно сортиране на всяка кофичка и обединяване обратно
-    int index = 0;
+    int ind = 0;
     for (int i = 0; i < 10; i++) {
-        if (bucketCounts[i] > 0) {
-            radix_sort(buckets[i], bucketCounts[i], digitIndex - 1);
-            for (int j = 0; j < bucketCounts[i]; j++) {
-                arr[index++] = buckets[i][j];
+        if (buckCount[i] > 0) {
+            radix_sort(bucket[i], buckCount[i], digitInd - 1);
+            for (int j = 0; j < buckCount[i]; j++) {
+                arr[ind++] = bucket[i][j];
             }
         }
-        free(buckets[i]);
+        free(bucket[i]);
     }
+    printf("Sort by position digit %d:\n", digitInd + 1);
+    printArray(arr, count);
 }
 
 int main() {
     int arr1[] = {5, 23, 45, 43, 10, 22, 55, 155, 125};
-    unsigned int count1 = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {12 ,889, 163, 8, 0, 12};
+    int arr3[] = {985, 745, 745, 845, 213, 79, 782, 857, 964, 535, 784, 951, 425, 0, 45};
+    int count = sizeof(arr1) / sizeof(arr1[0]);
+    int count2 = sizeof(arr2) / sizeof(arr2[0]);
+    int count3 = sizeof(arr3) / sizeof(arr3[0]);
 
-    printf("Начален масив:\n");
-    printArray(arr1, count1);
 
-    int maxDigits = getMaxDigits(arr1, count1);
-    radix_sort(arr1, count1, maxDigits - 1);
+    printf("Array:\n");
+    printArray(arr1, count);
 
-    printf("Сортиран масив:\n");
-    printArray(arr1, count1);
+    int maxDigits = mDigit(arr1, count);
+    radix_sort(arr1, count, maxDigits - 1);
+
+    printf("Sorted:\n");
+    printArray(arr1, count);
+
+
+    printf("\n\nArray:\n");
+    printArray(arr2, count2);
+
+    maxDigits = mDigit(arr2, count2);
+    radix_sort(arr2, count2, maxDigits - 1);
+
+    printf("Sorted:\n");
+    printArray(arr2, count2);
+    printf("\n\nArray:\n");
+    printArray(arr3, count3);
+
+    maxDigits = mDigit(arr3, count3);
+    radix_sort(arr3, count3, maxDigits - 1);
+
+    printf("Sorted:\n");
+    printArray(arr3, count3);
 
     return 0;
 }
